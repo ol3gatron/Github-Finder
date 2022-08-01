ac25452b298af78a37b652a43b786c28a933eb96
ol3gatron <olegatron@gmail.com>
2022-08-01 19:08:08 +0300

    clean up fetch users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -12,12 +12,13 @@ export const GithubProvider = ({ children }) => {
 
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
   async function fetchUsers() {
+    setLoading()
     const res = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -31,6 +32,9 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     })
   }
+
+  const setLoading = () => dispatch({ type: "SET_LOADING" })
+
   return (
     <GithubContext.Provider
       value={{
