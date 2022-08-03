9ee41b4cbb66c4a62a6321adb29513606a9310eb
ol3gatron <olegatron@gmail.com>
2022-08-03 16:11:04 +0300

    installed axios and cleaned up actions

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,19 +1,20 @@
+import axios from "axios"
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+})
 
 export async function searchUsers(text) {
   const params = new URLSearchParams({
     q: text,
   })
 
-  const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
-  const { items } = await res.json()
-
-  return items
+  const res = await github.get(`/search/users?${params}`)
+  return res.data.items
 }
 
 export async function getUser(login) {
@@ -31,17 +32,11 @@ export async function getUser(login) {
   }
 }
 
-export async function getUserRepos(login) {
-  const params = new URLSearchParams({
-    sort: "created",
-    per_page: 10,
-  })
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ])
 
-  const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
-  const data = await res.json()
-  return data
+  return { user: user.data, repos: repos.data }
 }
